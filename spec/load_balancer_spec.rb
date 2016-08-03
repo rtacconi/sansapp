209125e464b7eae0367d8f00585607d1d3d4d7bf
Riccardo Tacconi <rtacconi@gmail.com>
2016-08-03 19:37:23 +0100

    Moved port from 8080 to 8081

--- spec/load_balancer_spec.rb
+++ spec/load_balancer_spec.rb
@@ -6,8 +6,8 @@ describe 'Load Balancer' do
     # this test is not sensitive to the order of the output
     # but it checks if requests are alternated between the two backends
     (1..10).each do
-      first = Faraday.get('http://localhost:8080/').body
-      second = Faraday.get('http://localhost:8080/').body
+      first = Faraday.get('http://localhost:8081/').body
+      second = Faraday.get('http://localhost:8081/').body
       first_and_second = [ first, second ]
       # check if both servers are respoding
       expect(first_and_second).to include("Hi there, I'm served from app-node1-ubuntu-1604!")
